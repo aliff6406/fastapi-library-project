fda7df98e944ef48ea2e1893025198efb18032de
Aliff Amir <aliff6406@gmail.com@users.noreply.github.com>
2023-06-15 13:58:00 +0800

    add pydantic models 'Book' and 'User'

--- fastapi_project/models.py
+++ fastapi_project/models.py
@@ -1,2 +1,22 @@
-from pydantic import BaseModel
+from datetime import date, datetime
+from enum import Enum
+from uuid import UUID, uuid4
+from pydantic import BaseModel, Field
 
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+    
+class Book(BaseModel):
+    id: UUID = uuid4()
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1)
+    rating: int = Field(gt=-1, lt=101)
+    published_date: datetime.date = Field(lt=date.today)
+
+class User(BaseModel):
+    id: UUID = uuid4()
+    name: str = Field(min_length=1)
+    age: int = Field(gt=0)
+    gender: Gender
+    borrowed_books: list[Book] | None = []
