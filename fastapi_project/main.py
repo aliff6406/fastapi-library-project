154b6524f81df0da35aba7cc073b038f7f7d8a8a
Aliff Amir <aliff6406@gmail.com@users.noreply.github.com>
2023-06-16 00:42:38 +0800

    add FastAPI path operations to create and read data to and from database

--- fastapi_project/main.py
+++ fastapi_project/main.py
@@ -1,33 +1,60 @@
 from datetime import date
 import uvicorn
 from uuid import uuid4
-from fastapi import FastAPI
-from .schemas import Gender, User, Book
+from fastapi import FastAPI, Depends, HTTPException
+from . import crud, models, schemas
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from . import models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-# db_books: list[Book] = [
-#     Book(id=uuid4(), title="Jack and Jill", author="Louisa May Alcott", rating=99, published_date=date(2022, 1, 2)),
-#     Book(id=uuid4(), title="Jack and the Beanstalk", author="Joseph Jacobs", rating=85, published_date=date(2022, 1, 2))
-# ]
-
-# db_users: list[User] = [
-#     User(id=uuid4(), name="Aliff", age=20, gender=Gender.male, borrowed_books=[db_books[0], db_books[1]]),
-#     User(id=uuid4(), name="Hana", age=21, gender=Gender.female),
-# ]
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def root():
     return {"Message" : "Home Page"}
 
-@app.get("/users")
-def get_users():
-    return db_users
-    
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already Registered")
+    return crud.create_user(db=db, user=user)
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(db: Session = Depends(get_db)):
+    users = crud.get_users(db)
+    return users
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db=db, user_id=user_id)
+    return db_user
+
+@app.post("books/", response_model=schemas.Book)
+def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    db_book = crud.get_book_by_title(db, title=book.title)
+    if db_book:
+        raise HTTPException(status_code=400, detail="Book already Registered")
+    return crud.create_book(db=db, book=book)
+
+@app.get("/books/", response_model=list[schemas.Book])
+def read_books(db: Session = Depends(get_db)):
+    books = crud.get_books(db)
+    return books
+
+@app.get("/books/{book_id}", response_model=schemas.Book)
+def read_book(book_id: int, db: Session = Depends(get_db)):
+    db_book = crud.get_book(db=db, book_id=book_id)
+    return db_book
+
 def main():
     """Launched with `poetry run start`"""
     uvicorn.run("fastapi_project.main:app", host="localhost", port=8000, reload=True)
--- fastapi_project/models.py
+++ fastapi_project/models.py
@@ -3,19 +3,6 @@ from sqlalchemy.orm import relationship
 from .database import Base
 
 
-class Book(Base):
-    __tablename__ = "books"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, unique=True, index=True)
-    author = Column(String)
-    rating = Column(Integer)
-    published_date = Column(Date)
-    is_available = Column(Boolean, default=True)
-    borrower_id = Column(Integer, ForeignKey("User.id"))
-    
-    borrower = relationship("User", back_populates="book")
-
 class User(Base):
     __tablename__ = "users"
 
@@ -26,3 +13,16 @@ class User(Base):
     gender = Column(String)
 
     book = relationship("Book", back_populates="borrower")
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, unique=True, index=True)
+    author = Column(String)
+    rating = Column(Integer)
+    published_date = Column(Date)
+    is_available = Column(Boolean, default=True)
+    borrower_id = Column(Integer, ForeignKey("users.id"))
+    
+    borrower = relationship("User", back_populates="book")
