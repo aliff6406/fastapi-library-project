274a6e357c1f5b183d4e505ec70d86059dd92ec8
Aliff Amir <aliff6406@gmail.com@users.noreply.github.com>
2023-06-15 14:41:20 +0800

    add validators to verify published date of book and set maximum allowed number of borrowed books

--- fastapi_project/main.py
+++ fastapi_project/main.py
@@ -1,11 +1,29 @@
+from datetime import date
 import uvicorn
+from uuid import uuid4
 from fastapi import FastAPI
+from .models import Gender, User, Book
 
 app = FastAPI()
+
+
+db_books: list[Book] = [
+    Book(id=uuid4(), title="Jack and Jill", author="Louisa May Alcott", rating=99, published_date=date(2022, 1, 2)),
+    Book(id=uuid4(), title="Jack and the Beanstalk", author="Joseph Jacobs", rating=85, published_date=date(2022, 1, 2))
+]
+
+db_users: list[User] = [
+    User(id=uuid4(), name="Aliff", age=20, gender=Gender.male, borrowed_books=[db_books[0], db_books[1]]),
+    User(id=uuid4(), name="Hana", age=21, gender=Gender.female),
+]
 
 @app.get("/")
 def root():
     return {"Message" : "Home Page"}
+
+@app.get("/users")
+def get_users():
+    return db_users
     
 def main():
     """Launched with `poetry run start`"""
--- fastapi_project/models.py
+++ fastapi_project/models.py
@@ -1,18 +1,24 @@
-from datetime import date, datetime
+from datetime import date
 from enum import Enum
 from uuid import UUID, uuid4
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 class Gender(str, Enum):
     male = "male"
     female = "female"
-    
+
 class Book(BaseModel):
     id: UUID = uuid4()
     title: str = Field(min_length=1)
     author: str = Field(min_length=1)
     rating: int = Field(gt=-1, lt=101)
-    published_date: datetime.date = Field(lt=date.today)
+    published_date: date
+
+    @validator("published_date")
+    def verify_published_date(cls, v):
+        if v > date.today():
+            raise ValueError("date of publish cannot exceed today's date")
+        return v
 
 class User(BaseModel):
     id: UUID = uuid4()
@@ -20,3 +26,9 @@ class User(BaseModel):
     age: int = Field(gt=0)
     gender: Gender
     borrowed_books: list[Book] | None = []
+
+    @validator("borrowed_books")
+    def maximum_borrowed_books(cls, v):
+        if len(v) > 3:
+            raise ValueError("maximum allowed number of borrowed books is 3")
+        return v
